06496f62f5733baa0968fbd119fc7f12de230458
robottdog.c <robottdog.c@gmail.com>
2018-05-25 15:27:46 +0800

    完成基础架构

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
-import Koa from 'koa';
-import {Loader}  from './loader';
-
-const app = new Koa;
-const loader = new Loader(app);
-
-app.use(loader.loadFlow());
+import { Calabash } from "./calabash";
+import * as cluster from 'cluster';
 
 
-app.listen(3000, '127.0.0.1', () => {
-    console.log('服务器在运行');
-})
+
+if (cluster.isMaster) {
+    var numWorkers = require('os').cpus().length;
+
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+    for (var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', function (worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+    const app = new Calabash;
+
+    app.run(() => { });
+}
--- src/loader.ts
+++ src/loader.ts
@@ -5,56 +5,86 @@ import { BaseContext } from 'koa';
 import { deco } from './decorator';
 import { Calabash } from './calabash';
 
-export class Loader {
-  router: Router = new Router;
-  controller: any = {};
-  app: any;
+const HASLOADED = Symbol('hasloaded');
 
-  constructor(app: any) {
+interface FileModule {
+  module: any;
+  filename: string;
+}
+
+interface StringSub {
+  source: string;
+  isFound: boolean;
+}
+
+interface Plugin {
+  enable: boolean,
+  package: string
+}
+
+
+function removeString(source: string, str: string): StringSub {
+  const index = source.indexOf(str);
+  if (index > 0) {
+    return {
+      source: source.substr(0, index),
+      isFound: true
+    };
+  }
+  return {
+    source: source,
+    isFound: false
+  };
+}
+
+export class Loader {
+  private router: any = new Router;
+  private app: Calabash;
+  controller: any = {};
+
+  constructor(app: Calabash) {
     this.app = app;
+  }
+
+  private appDir() {
+    const subString = removeString(__dirname, 'node_modules');//如果包在node_modules中，正式环境
+    if (subString.isFound) {
+      return subString.source;
+    }
+    return subString.source.substr(0, subString.source.length - 4) + '/';
+  }
+
+  private fileLoader(url: string): Array<FileModule> {
+    const merge = this.appDir() + url; // app/controller
+
+    return fs.readdirSync(merge).map((name) => {
+      return {
+        module: require(merge + '/' + name).default,
+        filename: name
+      };
+    });
   }
 
   loadFlow(){
     this.loadConfig();
-    this.loadService();
+    this.loadPlugin();
     this.loadController();
-    return this.loadRouter();
-  }
-
-  loadService() {
-    const service = fs.readdirSync(__dirname + '/service');
-    var that = this;
-    Object.defineProperty(this.app.context, 'service', {
-      get() {
-        if (!(<any>this)['cache']) {
-          (<any>this)['cache'] = {};
-        }
-        const loaded = (<any>this)['cache'];
-
-        // If some services failed to be loaded ? 
-        if (!loaded['service']) {
-          loaded['service'] = {};
-          service.forEach((d) => {
-            const name = d.split('.')[0];
-            const mod = require(__dirname + '/service/' + d);
-            loaded['service'][name] = new mod(this, that.app);
-          });
-          return loaded.service;
-        }
-        return loaded.service;
-      }
-    });
+    this.loadService();
+    this.loadMiddleware();
+    this.loadRouter();//依赖loadController 
   }
 
   loadController(){
-    const dirs = fs.readdirSync(__dirname + '/controller');
-    dirs.forEach((filename) => {
-      require(__dirname + '/controller/' + filename).default;
-    })
+    this.fileLoader('app/controller');
+  }
+
+  loadService() {
+    const service = this.fileLoader('app/service');
+    this.loadToContext(service, this.app, 'service');
   }
 
   loadRouter() {
-    const routes = decorator.getRoutes();
+    const routes = deco.getRoutes();
     Object.keys(routes).forEach((route) => {
       routes[route].forEach((deco) => {
         (<any>this.router)[deco.method](route, async (ctx: BaseContext) => {
@@ -63,31 +93,71 @@ export class Loader {
         })
       })
     })
-    return this.router.routes();
+    this.app.use(this.router.routes());
   }
 
-  loadConfig() {
-    const defConfig = require(__dirname + '/config/config.default.js');
-    const envConfig = require(__dirname + 
-      (process.env.NODE_ENV === 'production' ? '/config/config.default.js':'/config/config.dev.js')
-    );
-    const config = Object.assign({}, defConfig, envConfig);
-    Object.defineProperty(this.app, 'config', {
-      get: () => {
-        return config;
-      }
-    });
-  }
-
-  loadPlugin() {
-    const pluginModule = require(__dirname + '/config/plugin.js');
-    Object.keys(pluginModule).forEach((key) => {
-      // pluginModule[key];
-      if (pluginModule[key] && pluginModule[key].enable) { //判断是否开启
-        const plugin = require(pluginModule[key].packagePath);
-        plugin(this.app);
+  loadToContext(target: Array<FileModule>, app: Calabash, property: string) {
+    Object.defineProperty(app.context, property, {
+      get() {
+        if (!(<any>this)[HASLOADED]) {
+          (<any>this)[HASLOADED] = {};
+        }
+        const loaded = (<any>this)[HASLOADED];
+        if (!loaded[property]) {
+          loaded[property] = {};
+          target.forEach((mod) => {
+            const key = mod.filename.split('.')[0];
+            loaded[property][key] = new mod.module(this, app);
+          })
+          return loaded.service
+        }
+        return loaded.service;
       }
     })
   }
 
+  loadMiddleware() {
+    try {
+      const middleware = this.fileLoader('app/middleware');
+      const registedMid = this.app.config['middleware'];
+
+      if (!registedMid) return;//如果中间件不存在
+      registedMid.forEach((name: string) => {
+        logger.blue(name);
+        for (const index in middleware) {
+          const mod = middleware[index];
+          const fname = mod.filename.split('.')[0];
+          if (name === fname) {
+            this.app.use(mod.module());
+          }
+        }
+      })
+    } catch (e) { }
+  }
+
+  loadConfig() {
+    const configDef = this.appDir() + 'app/config/config.default.js';
+    const configEnv = this.appDir()
+      + (process.env.NODE_ENV === 'production' ? 'app/config/config.pro.js' : 'app/config/config.dev.js');
+    const conf = require(configEnv).default;
+    const confDef = require(configDef).default;
+    const merge = Object.assign({}, conf, confDef);
+    Object.defineProperty(this.app, 'config', {
+      get: () => {
+        return merge
+      }
+    })
+  }
+
+  loadPlugin() {
+    const Pdir = this.appDir() + 'app/config/plugin.js';
+    const plugins = require(Pdir).default;
+    for (const index in plugins) {
+      const plugin: Plugin = plugins[index];
+      if (plugin.enable) {
+        const pkg = require(plugin.package);
+        pkg(this.app);
+      }
+    }
+  }
 }
